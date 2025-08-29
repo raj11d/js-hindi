9a8375723624e40b983f5ee3730f7a45f552a7c7
raj11d <160199574+raj11d@users.noreply.github.com>
2025-08-29 16:33:21 +0000

    datatype done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert("Hello")
+
+//alert (3 + 3) // we are using nodejs, not browser
+
+let name = "Harshit"
+let age = 18
+let isLoggedId = false
+
+//Data Type
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true / false
+// null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
